27ef0448ae9f26912624bd862f1cc605e02d63e0
Simba Wangu <simbawangulu@gmail.com>
2023-01-31 11:49:56 +0300

    update repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <h1 className={styles.h1}>Vite + React</h1>
-    </div>
+    <div>App</div>
   )
-}
-
-const styles = {
-  h1: 'text-3xl font-bold underline'
 }
 
 export default App
