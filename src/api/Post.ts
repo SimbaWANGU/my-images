52456b9d0872652fa6800e4d71972938405e586f
Simba Wangu <simbawangulu@gmail.com>
2023-02-07 16:31:39 +0300

    add post method for generating images

--- src/api/Post.ts
+++ src/api/Post.ts
@@ -1,21 +1,23 @@
-const generateImage = async (email: string, prompt: string, number: number): Promise<any> => {
+const generateImage = async (prompt: string, number: number): Promise<any> => {
   let data
-  const response = await fetch('https://my-images.onrender.com/', {
+  const response = await fetch('https://my-images.onrender.com/image/create', {
     method: 'post',
     headers: {
       'Content-type': 'application/x-www-form-urlencoded'
     },
-    body: `email=${email}&prompt=${prompt}&nun_images=${number}`
+    body: `prompt=${prompt}&num_images=${number}`
   })
   switch (response.status) {
     case 200:
     case 201:
       data = await response.json()
       return data
+
     case 400:
     case 500:
       data = await response.json()
       console.log(data.error)
+      break
   }
 }
 
