3c822ba9f7183a18341e2d599cb334f3bf762feb
Simba Wangu <simbawangulu@gmail.com>
2023-02-13 13:59:44 +0300

    dynamically render images in Images.tsx

--- src/containers/MyImages/Images.tsx
+++ src/containers/MyImages/Images.tsx
@@ -4,155 +4,39 @@ import { Autoplay, EffectCards } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/effect-cards'
 import ImageDiv from '../../components/ImageDiv'
+import { photos1, photos2, photos3, photos4, photos5 } from '../../assets/exports'
 
 const Images = () => {
-  const src= 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'
   const styles = {
     div: 'bg-weird-blue px-14 text-white h-full w-full grid gap-4 grid-cols-3 grid-rows-2',
     swiper: 'h-48 w-48',
     classes: 'h-48 w-48 object-cover rounded-xl',
     Imagediv: 'h-48 w-48 border rounded-xl'
   }
+  const photosArray = [photos1, photos2, photos3, photos4, photos5]
+  const delay = [3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000]
+
   return (
     <div className={styles.div}>
-      <div>
+      {photosArray.map((photos) => (
+      <a href='#' key={photosArray.findIndex((item) => item === photos)}>
         <Swiper
           effect={'cards'}
           grabCursor={true}
           autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
+            delay: delay[Math.floor(Math.random() * delay.length)],
           }}
           modules={[EffectCards, Autoplay]}
           className={styles.swiper}
         >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
+          {photos.map((pics) => (
+            <SwiperSlide key={photos1.findIndex((item) => item === pics)}>
+              <ImageDiv src={pics} classes={styles.classes} div={styles.Imagediv} />
+            </SwiperSlide>
+          ))}
         </Swiper>
-      </div>
-      <div>
-        <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
-          }}
-          modules={[EffectCards, Autoplay]}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div>
-      <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
-          }}
-          modules={[EffectCards, Autoplay]}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div>
-      <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
-          }}
-          modules={[EffectCards, Autoplay]}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div>
-      <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
-          }}
-          modules={[EffectCards, Autoplay]}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div>
-      <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          autoplay={{
-            delay: 5000,
-            disableOnInteraction: true
-          }}
-          modules={[EffectCards, Autoplay]}
-          className={styles.swiper}
-        >
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes} div={styles.Imagediv} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ImageDiv src={src} classes={styles.classes}  div={styles.Imagediv}/>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+      </a>
+      ))}
     </div>
   )
 }
